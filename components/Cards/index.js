cc23a12258601b37dc2098681f0ad8f6da09a572
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-02-14 09:03:58 -0800

    Not sure if my previous commit saved?

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardCont = document.querySelector(".cards-container");
+
+axios("https://lambda-times-backend.herokuapp.com/articles")
+  .then(res => {
+    const articles = res.data.articles;
+    const articlesKeys = Object.keys(articles);
+    console.log("These are the articles", articles);
+    console.log("These are the article keys", Object.keys(articles));
+    // using obj.key as [notation] to access each article obj
+    articlesKeys.forEach(ele => {
+      // all Object Arrays are finally parsed down to be card post
+      articles[ele].forEach(element => {
+        // console.log(element);
+        cardMaker(element);
+      });
+      // console.log(articles[ele]);
+    });
+  })
+  .catch(err => console.error(err));
+
+function cardMaker(props) {
+  const card = document.createElement("div");
+  const headTitle = document.createElement("div");
+  const author = document.createElement("div");
+  const imgCont = document.createElement("div");
+  const img = document.createElement("img");
+  const byAuthor = document.createElement("span");
+
+  card.classList.add("card");
+  headTitle.classList.add("headline");
+  author.classList.add("author");
+  imgCont.classList.add("img-container");
+
+  headTitle.textContent = props.headline;
+  byAuthor.textContent = props.authorName;
+  img.src = props.authorPhoto;
+
+  card.appendChild(headTitle);
+  card.appendChild(author);
+  author.appendChild(imgCont);
+  imgCont.appendChild(img);
+  author.appendChild(byAuthor);
+  cardCont.appendChild(card);
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerCont = document.querySelector(".header-container");
+
+function Header() {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const classYear = document.createElement("span");
+
+  header.classList.add("header");
+  date.classList.add("date");
+  classYear.classList.add("temp");
+
+  date.textContent = `March 28, 2019`;
+  title.textContent = `Lambda Times`;
+  classYear.textContent = `98°`;
+
+  headerCont.appendChild(header);
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(classYear);
+}
+
+Header();
+
+function currentDate() {}
